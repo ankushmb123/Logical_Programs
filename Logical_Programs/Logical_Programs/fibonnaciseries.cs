0e47cf682673c5d0a822d05090df105c9809c505
Ankush Bahekar <ankushmb9@gmail.com>
2022-04-27 09:47:09 +0530

    [Ankush] Add: Fibonnaci Series

---
+++ Logical_Programs/Logical_Programs/fibonnaciseries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programs
+{
+    public class fibonnaciseries
+    {
+        public static void Displayfibonnaciseries()
+        {
+            int Number1 = 0, Number2 = 1, Number3, i, Number;
+            Console.WriteLine("Enter Number : ");
+            Number = int.Parse(Console.ReadLine());
+            Console.Write(Number1 + " " + Number2 + " ");
+            for (i = 2; i < Number; i++)
+            {
+                Number3 = Number1 + Number2;
+                Console.Write(Number3 + " ");
+                Number1 = Number2;
+                Number2 = Number3;
+            }
+
+        }
+    }
+}
